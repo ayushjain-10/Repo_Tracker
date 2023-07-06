1057c25ca27160b2eb689faebc50350cb2dc5577
Ayush Jain <ayushshreeshreemal@gmail.com>
2023-07-06 16:08:15 -0700

    Sends the data to a json file

--- scrape.go
+++ scrape.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"github.com/gocolly/colly"
 )
 
+type Repository struct {
+	Link string `json:"link"`
+}
+
 func main() {
+	var repositories []Repository
+
 	// Instantiate default collector
 	c := colly.NewCollector(
 		// Visit only domains: github.com, www.github.com
@@ -16,13 +24,19 @@ func main() {
 
 	// Callback for when visiting a repository page
 	c.OnHTML("div[id='user-repositories-list'] div h3 a", func(e *colly.HTMLElement) {
-		repoLink := e.Attr("href")
-		fmt.Printf("Found repository: https://github.com%s\n", repoLink)
+		repoLink := "https://github.com" + e.Attr("href")
+		repositories = append(repositories, Repository{Link: repoLink})
 	})
 
 	// Callback for handling scraped data
 	c.OnScraped(func(r *colly.Response) {
 		fmt.Println("Finished scraping the page", r.Request.URL)
+
+		// After finished scraping, save our results to a .json file
+		file, _ := json.MarshalIndent(repositories, "", " ")
+
+		_ = ioutil.WriteFile("repositories.json", file, 0644)
+		fmt.Println("Saved results to repositories.json")
 	})
 
 	// Start the scraping process
